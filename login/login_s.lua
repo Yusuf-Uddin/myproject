8875417f52e5f246ce5bbac40b6f9442c51ff704
Yusuf Laptop <yusuf-uddin1990@gmx.de>
2020-04-10 15:25:59 +0200

    add register account

--- login/login_s.lua
+++ login/login_s.lua
@@ -6,6 +6,21 @@ function login(type, username, password)
     -- Register new Account
     elseif (type == 'Register') then
 
+        -- Check if username already exists
+        if getAccount(username) then
+            return outputChatBox('Username already in use.')
+        end
+        
+        -- Hash the password
+        passwordHash(password, 'bcrypt', {}, function(hashedPassword)
+            
+            -- Create the account
+            local account = addAccount(username, hashedPassword)
+            setAccountData(account, 'hashed_password', hashedPassword)
+            dbExec(connection, 'INSERT INTO userdata (username, password, money, faction_id, x, y, z, new) VALUES(?,?,?,?,?,?,?,?)', username, hashedPassword, 0, 1, 0, 0, 5, true)
+            outputChatBox('Account successfully created.')
+
+        end)
     end
 end
 addEvent("handleLogin", true)
