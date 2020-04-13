c9ddd60fd1e2a4e89df99fbdf25cd47d358c4e44
Yusuf Laptop <yusuf-uddin1990@gmx.de>
2020-04-13 11:01:39 +0200

    start ped selection

--- chatSys/admin_s.lua
+++ chatSys/admin_s.lua
@@ -1,11 +1,23 @@
+local admin_ranks = {
+    ['Admin'] = '#FF0000',
+    ['Developer'] = '#FF0010',
+    ['Moderator'] = '#FF0010',
+    ['Supporter'] = '#FF0010'
+}
+
 function rolePrefix(msg, msgType)
 local admin_rank = getElementData(source, 'admin_rank')
+
     -- Dont sent the original message
     cancelEvent()
 
     -- Send message with rank prefix
-    if (admin_rank == 'Admin') then
-        outputChatBox('#FF0000[ADMIN]'..getPlayerName(source)..' #FFFFFF'..msg, root, 255, 255, 255, true)
+    for i, v in pairs(admin_ranks) do
+        if (i == admin_rank) then
+            local uppercaseRank = string.upper(i)
+            outputChatBox(v..'['..uppercaseRank..']#FFFFFF'..getPlayerName(source)..': #FFFFFF'..msg, root, 255, 255, 255, true)
+            break
+        end
     end
 end
 addEventHandler('onPlayerChat', root, rolePrefix)
